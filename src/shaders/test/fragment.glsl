15d91d3818b165ee75e72e3514c653ddf0e0dd06
zoyron <sgrarora3@gmail.com>
2024-04-15 12:59:54 +0530

    done another pattern

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -1,7 +1,10 @@
 varying vec2 vUv;
 
 void main(){
-  float strength = mod(vUv.y * 10.0, 1.0);
-  strength = step(0.75, strength);
+  float strength = step(0.4, mod(vUv.y * 10.0, 1.0));
+  strength *= step(0.75, mod(vUv.x * 10.0, 1.0));
+  float strength1 = step(0.4, mod(vUv.x * 10.0, 1.0));
+  strength1 *= step(0.75, mod(vUv.y * 10.0, 1.0));
+  strength += strength1;
   gl_FragColor = vec4(vec3(strength), 1.0);
 }
