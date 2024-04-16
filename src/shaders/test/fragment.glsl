61951ac804c14a7ba04d6cca10f8d7bece391bee
zoyron <sgrarora3@gmail.com>
2024-04-17 03:25:35 +0530

    30 patterns done

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -1,7 +1,12 @@
 varying vec2 vUv;
 
+float random (vec2 st) {
+    return fract(sin(dot(st.xy,
+                         vec2(12.9898,78.233)))*
+        43758.5453123);
+}
+
 void main(){
-  float strength = ceil(vUv.x * 10.0) / 10.0;
-  strength *= ceil(vUv.y * 10.0) / 10.0;
+  float strength = 0.015/(distance(vec2(vUv.y), vec2(0.5)));
   gl_FragColor = vec4(vec3(strength), 1.0);
 }
